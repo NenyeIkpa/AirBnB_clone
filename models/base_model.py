8d3b0e7ee924d13cceed3b88125855e928fac440
NenyeIkpa <nenyeikpa@gmail.com>
2023-11-08 19:43:31 +0000

    Task 4- created base model from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,25 @@ from datetime import datetime
 class BaseModel:
     """ parent class, BaseModel """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ initialization function for an object of the BaseModel class """
-        # assign a unique id to each object
-        BaseModel.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "created_at":
+                    self.created_at = datetime.fromisoformat(value)
+                if key == "updated_at":
+                    self.updated_at = datetime.fromisoformat(value)
+        else:
+            # assign a unique id to each object
+            self.id = str(uuid.uuid4())
+
+            date_format = '%Y-%m-%dT%H:%M:%S.%f'
+            date_now = datetime.now()
+            self.created_at = date_now.strftime(date_format)
+            self.updated_at = self.created_at
 
     def __str__(self):
         """ returns a string representation the object """
@@ -27,5 +41,7 @@ class BaseModel:
     def to_dict(self):
         """ returns a dictionary representation of the object """
         obj_dict = self.__dict__
+        obj_dict['created_at'] = str(self.created_at)
+        obj_dict['updated_at'] = str(self.updated_at)
         obj_dict['__class__'] = self.__class__.__name__
         return obj_dict
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,8 +17,13 @@ class TestBaseModel(unittest.TestCase):
 
     def test_args(self):
         """tests for args passed to BaseModel """
-        with self.assertRaises(TypeError):
-            BaseModel(121)
+        """
+            bm2 = BaseModel({'my_number': 89, 'name': 'My First Model', '__class__': 'BaseModel', 'updated_at': '2017-09-28T21:05:54.119572',
+            'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28T21:05:54.119427'})
+            self.assertEqual(bm2.created_at, datetime(2017, 9, 28, 21, 5, 54, 119427))
+        """
 
-        with self.assertRaises(TypeError):
-            BaseModel("abx")
+        """
+            with self.assertRaises(TypeError):
+            BaseModel([89, 'My First Model', 'BaseModel', '2017-09-28T21:05:54.119572', 'b6a6e15c-c67d-4312-9a75-9d084935e579', '2017-09-28T21:05:54.119427'])
+        """
